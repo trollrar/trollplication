a094c53ffb3cb17103222c749942ed43258c2cfe
fares <troll.rar@gmail.com>
2020-10-11 23:06:25 +0200

    style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {Observable} from 'rxjs';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {map} from 'rxjs/operators';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {auth} from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +14,16 @@ export class AppComponent {
   public currentMessage: string;
 
   items: Observable<any[]>;
+  authState: firebase.User;
 
-  constructor(private firestore: AngularFirestore) {
+  constructor(private firestore: AngularFirestore, private fireAuth: AngularFireAuth) {
     this.items = firestore.collection('items').valueChanges().pipe(
       map(messages => messages.sort((a, b) => (a as any).timestamp - (b as any).timestamp))
     );
+    fireAuth.authState.subscribe( authState => {
+      this.authState = authState;
+    });
   }
-
   // tslint:disable-next-line:typedef
   public async sendMessage() {
     if (!this.currentMessage || this.currentMessage === '') {
@@ -27,5 +32,19 @@ export class AppComponent {
     const msg = this.currentMessage;
     this.currentMessage = '';
     await this.firestore.collection('items').add({message: msg, timestamp: new Date()});
+  }
+
+
+  public login(): void {
+    const user = new auth.GoogleAuthProvider();
+    this.fireAuth.signInWithPopup(user);
+  }
+
+  public logout(): void {
+    this.fireAuth.signOut();
+  }
+
+  public test(): void {
+    console.log(this.authState.email);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAYKpKz5v8jCUOFpuT_HLABc7w7ia1F_-s',
+    authDomain: 'trollplication.firebaseapp.com',
+    databaseURL: 'https://trollplication.firebaseio.com',
+    projectId: 'trollplication',
+    storageBucket: 'trollplication.appspot.com',
+    messagingSenderId: '374848985895',
+    appId: '1:374848985895:web:874465a600afbfa35d2702',
+    measurementId: 'G-VRRZM8K8XQ'
+  }
 };
